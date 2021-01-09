721435dd3e1d1f4a2a0208fdaf4c5eed30ba417a
manuel <jgmanuelrivera00@gmail.com>
2021-01-09 18:16:10 -0500

    de vuelta al home

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
 class HomePageView(TemplateView):
-    template_name = "core/index.html"
+    template_name = "core/home.html"
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'title':"Mi Inventario"})
         
